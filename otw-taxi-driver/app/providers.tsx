c2622b55ca2cd55a4b13a42e20c48f5738502b53
dekana576 <dekana576@gmail.com>
2026-03-09 09:25:16 +0800

    set up Hero UI

--- otw-taxi-driver/app/providers.tsx
+++ otw-taxi-driver/app/providers.tsx
@@ -2,10 +2,16 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import * as React from "react";
+
+// 1. import `HeroUIProvider` component
+import { HeroUIProvider } from "@heroui/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = useState(() => new QueryClient())[0];
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </QueryClientProvider>
   );
 }
