e8c742b269ae75de13f171e33242805ffb13cf23
dekana576 <dekana576@gmail.com>
2026-03-09 16:50:39 +0800

    add create driver page

---
+++ otw-taxi-driver/app/create-driver/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function CreateDriver() {
+  return (
+    <div className="w-full text-center">
+      <p className="text-5xl">
+        <strong>FORM CREATE DRIVER</strong>
+      </p>
+    </div>
+  );
+}
