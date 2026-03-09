43889386100f43cf51d6299a64e78e9ae0417521
dekana576 <dekana576@gmail.com>
2026-03-09 14:38:23 +0800

    get data driver

--- otw-taxi-driver/app/page.tsx
+++ otw-taxi-driver/app/page.tsx
@@ -1,38 +1,82 @@
 "use client";
 
-import { Button, Card, CardBody, CardHeader } from "@heroui/react";
-import Image from "next/image";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Image,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@heroui/react";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+const base_url =
+  "https://22f766af-a68f-4e84-bab4-b02cde04069a.mock.pstmn.io/admin/users";
+
+interface Iuser {
+  user_id: string;
+  name: string;
+  email: string;
+  gender: string;
+}
 
 export default function Home() {
+  const queryClient = useQueryClient();
+
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await axios.get<{ data: Iuser[] }>(base_url);
+      return response.data.data;
+    },
+  });
   return (
-    <div className="w-full h-200 flex justify-center items-center">
-      <div className="w-100">
-        <Card>
-          <CardHeader>
-            <div className="items-center justify-center flex flex-col">
-              <Button variant="ghost" color="danger">
-                TES HERO UI
-              </Button>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                debitis suscipit corrupti ipsam quae, assumenda labore minima.
-                Dignissimos adipisci nesciunt debitis maxime expedita dicta
-                nihil a. Recusandae illum obcaecati quae!
-              </p>
-            </div>
-          </CardHeader>
-          <CardBody>
-            <h1>Card BODY</h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
-              animi illum modi eum voluptatem rerum quae nesciunt. Culpa
-              inventore officia cum dicta nulla fugiat laboriosam temporibus
-              laborum reprehenderit obcaecati. Fugiat!
-            </p>
-          </CardBody>
-        </Card>
+    <div className="w-full justify-center items-center flex flex-col">
+      <div className="w-200">
         <div className="h-100 overflow-clip justify-center items-center flex rounded-2xl my-5">
           <Image src="img/image.png" alt="" className="" />
+        </div>
+
+        <div className="my-5">
+          <Card>
+            <CardHeader>
+              <div className="w-full flex items-center justify-end">
+                <Button className="m-3" variant="ghost" color="warning">Create Driver</Button>
+              </div>
+            </CardHeader>
+
+            <CardBody>
+              <Table isStriped aria-label="Example static collection table">
+                <TableHeader>
+                  <TableColumn>NAME</TableColumn>
+                  <TableColumn>EMAIL</TableColumn>
+                  <TableColumn>GENDER</TableColumn>
+                  <TableColumn>ACTION</TableColumn>
+                </TableHeader>
+                <TableBody items={users ?? []}>
+                  {(user) => (
+                    <TableRow key={user.user_id}>
+                      <TableCell>{user.name}</TableCell>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.gender}</TableCell>
+                      <TableCell>+++</TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+            </CardBody>
+          </Card>
         </div>
       </div>
     </div>
